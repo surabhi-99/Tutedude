f3d726f266b8f477ef37c64c00fb8e40901be748
surabhi-99 <surabhithakur.33@gmail.com>
2022-11-19 17:16:55 +0530

    created referral-code

--- src/components/referral-code.jsx
+++ src/components/referral-code.jsx
@@ -2,9 +2,11 @@ import { Box, Grid, Typography } from "@mui/material";
 
 const referSummary = (props) => {
     return (
-        <Typography variant="subtitle2" color="primary">Referral Earning</Typography>
+       
         <Box>
-            <border
+              <Box component="span" sx={{ p: 2, border: '1px solid linear-gradient(270deg, #800080 0%, #FF864C 100%)' }}>
+              <Typography variant="subtitle2" color="primary"></Typography>
+              </Box>
         </Box>
     )
 }
